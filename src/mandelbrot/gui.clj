10ca6b140e5b320951cc2882c6e0c9444936737f
Alex Collins <alextcollins@gmail.com>
2012-10-21 11:08:01 +0100

    Move to main method for GUI rendering

--- src/mandelbrot/gui.clj
+++ src/mandelbrot/gui.clj
@@ -3,26 +3,33 @@
   (:import (java.awt BorderLayout Dimension Graphics Color))
   (:import (javax.swing JFrame JPanel)))
 
-(def frame (JFrame. "Mandelbrot in Clojure"))
+;(def frame (JFrame. "Mandelbrot in Clojure"))
 
-(def canvas 
-  (proxy [javax.swing.JPanel] []
-    (paintComponent [#^java.awt.Graphics g]
-        (let [points (build 10.0 20.0)]
-          (map #(.drawRect (%1 0) (%1 1) 1 1) points)))))
+;(def canvas 
+  ;(proxy [javax.swing.JPanel] []
+    ;(paintComponent [#^java.awt.Graphics g]
+        ;(let [points (build 10.0 20.0)]
+          ;(map #(do (println %1) (.drawRect (%1 0) (%1 1) 1 1)) points)))))
  
-(doto canvas
-    (.setPreferredSize (Dimension. 800 700)))
+;(doto canvas
+    ;(.setPreferredSize (Dimension. 800 700)))
 
-(doto frame
-    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-    (.setContentPane canvas)
-    (.pack) 
-    (.setVisible true))
+;(doto frame
+    ;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+    ;(.setContentPane canvas)
+    ;(.pack) 
+    ;(.setVisible true))
 
-;(defn draw 
-  ;"Draws the Mandelbrot fractal"
-  ;[]
-  ;(doto ))
-;
-;(draw)
+(defn -main
+  []
+  (let [frame (JFrame. "Mandelbrot in Clojure")
+        canvas (proxy [javax.swing.JPanel] []
+                (paintComponent [#^java.awt.Graphics g]
+                  (let [points (build 10.0 20.0)]
+                    (map #(do (println "Point is " %1) (.drawRect (%1 0) (%1 1) 1 1)) points))))
+       ]
+    (doto frame
+        (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+        (.setContentPane canvas)
+        (.pack) 
+        (.setVisible true))))
