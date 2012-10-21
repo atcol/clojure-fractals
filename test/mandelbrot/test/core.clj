90875d574a3c71c0c6127c4b3ed4aef477ea5b5f
Alex Collins <alextcollins@gmail.com>
2012-10-21 11:09:09 +0100

    Introduce I to in?. Add tests for iter-z

--- test/mandelbrot/test/core.clj
+++ test/mandelbrot/test/core.clj
@@ -2,14 +2,19 @@
   (:use [mandelbrot.core])
   (:use [clojure.test]))
 
+(def ITERS 50)
+
+(deftest test-iter-z
+  (is (= 2.0 (iter-z 0 1 1))))
+
 (deftest test-in-false 
-  (is (= false (in? [0, 0]))))
+  (is (= false (in? [0, 0] ITERS))))
 
 (deftest test-in-true 
-  (is (in? [-1, -1]))
-  (is (in? [-1.1, -1.1]))
-  (is (in? [-1.3, 1.3]))
-  (is (in? [-1.38, -1.38])))
+  (is (in? [-1, -1] ITERS))
+  (is (in? [-1.1, -1.1] ITERS))
+  (is (in? [-1.3, 1.3] ITERS))
+  (is (in? [-1.38, -1.38] ITERS)))
          ; c = 0, -1, -1.1, -1.3, -1.38
 
 (deftest test-mag
@@ -21,4 +26,5 @@
   (is (not (empty? (build 1 100))) "non-empty set"))
 
 (println (build -10.0 -1.0))
+(println (build 10.0 20.0))
   
