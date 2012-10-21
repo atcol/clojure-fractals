83e7a7acf696e990e1af34395aa14270165da8c1
Alex Collins <alextcollins@gmail.com>
2012-10-21 11:14:49 +0100

    Set GUI as the main class

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject mandelbrot "1.0.0-SNAPSHOT"
   :description "A renderer of the Mandelbrot set"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :aot [mandelbrot.core]
-  :main mandelbrot.core)
+  :aot [mandelbrot.gui]
+  :main mandelbrot.gui)
