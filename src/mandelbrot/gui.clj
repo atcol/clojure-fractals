44fb2256a60e29d215371f4ca52523744efd57b3
Alex Collins <alextcollins@gmail.com>
2013-04-19 22:22:35 +0100

    Fill Frame with Panel; simplify drawing logic

--- src/mandelbrot/gui.clj
+++ src/mandelbrot/gui.clj
@@ -1,6 +1,6 @@
 (ns mandelbrot.gui
   (:use [mandelbrot.core])
-  (:import (java.awt Dimension))
+  (:import (java.awt Dimension BorderLayout))
   (:import (javax.swing JFrame JPanel)))
 
 (def WINDOW_SIZE (Dimension. 1024 768))
@@ -14,23 +14,21 @@
   (let [frame (JFrame. "Mandelbrot in Clojure")
         panel (proxy [javax.swing.JPanel] []
                 (paintComponent [#^java.awt.Graphics g]
-                  (def x1 -800.0)
-                  (def y1 -800.0)
-                  (def x2 WIDTH)
-                  (def y2 HEIGHT)
                   (dotimes [i WIDTH]
                     (dotimes [j HEIGHT]
-                      (def xN (* (+ x1 i) scale))
-                      (def yN (* (+ y1 j) scale))
+                      (def xN (* i scale))
+                      (def yN (* j scale))
                       (if (in? [xN yN] MAX_ITER)
                         (do 
                           ;(println xN "," yN)
-                          (.drawRect g (+ (/ WIDTH 2) (/ xN scale)) (+ (/ HEIGHT 2) (/ yN scale)) 1 1)))))))]
+                          (.drawRect g i j 1 1)))))))]
 
     (doto panel
       (.setPreferredSize WINDOW_SIZE))
     (doto frame
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.add panel)
+      (.setPreferredSize WINDOW_SIZE)
+      (.setLayout (BorderLayout.))
+      (.add panel BorderLayout/CENTER)
       (.pack) 
       (.setVisible true))))
